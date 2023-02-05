a1f862bca93171fc9547f1513873d5bb1ee817f7
EC2 Default User <121584968+Shinji20221229@users.noreply.github.com>
2023-02-05 08:48:34 +0000

    commit

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -10,8 +10,8 @@ class RelationshipsController < ApplicationController
   end
 
   def followings
-    user = User.find(params[:user_id])
-    @users = user.followings
+    @user = User.find(params[:user_id])
+    @users = @user.followings
   end
 
   def followers
